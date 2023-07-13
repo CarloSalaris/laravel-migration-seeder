1691ed9d85cf0852455552780c89971882354e2d
CarloSalaris <carloa.salaris@gmail.com>
2023-07-13 16:26:59 +0200

    Created table 'trains' and synced it with db

--- database/migrations/2023_07_13_122517_create_trains_table.php
+++ database/migrations/2023_07_13_122517_create_trains_table.php
@@ -14,11 +14,19 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
-            $table->id();
+            $table -> id();
 
+            $table -> string('azienda', 64);
+            $table -> string('stazione_partenza', 64);
+            $table -> string('stazione_arrivo', 64);
+            $table -> time('orario_partenza');
+            $table -> time('orario_arrivo');
+            $table -> string('codice', 10) -> unique();
+            $table -> integer('numero_carrozze');
+            $table -> boolean('in_orario') -> default(true);
+            $table -> boolean('cancellato') -> default(false);
 
-
-            $table->timestamps();
+            $table -> timestamps();
         });
     }
 
